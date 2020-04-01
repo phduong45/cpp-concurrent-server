d9e98def2d11b73bd9322ef7438496dc27d48070
Phuong Duong <phuongx31@gmail.com>
2020-04-01 14:21:18 +0700

    add client echo back

--- client.cpp
+++ client.cpp
@@ -5,6 +5,26 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+bool write_all(int fd, const char* data, std::size_t size) {
+    std::size_t total = 0;
+    while (total < size) {
+        ssize_t bytes_written = write(fd, data + total, size - total);
+
+        if (bytes_written == -1) {
+            if (errno != EINTR) {
+                return false;
+            } else {
+                continue;
+            }
+        }
+        if (bytes_written == 0) {
+            return false;
+        }
+        total += bytes_written;
+    }
+    return true;
+}
+
 int main() {
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd == -1) {
@@ -20,8 +40,45 @@ int main() {
     int result =
         connect(socket_fd, reinterpret_cast<const sockaddr*>(&server_address),
                 sizeof(server_address));
+
     if (result == -1) {
         std::cerr << "connect failed: " << std::strerror(errno) << "\n";
+        close(socket_fd);
+        return 1;
+    }
+
+    const char message[] = "hello from client\n";
+
+    if (!write_all(socket_fd, message, std::strlen(message))) {
+        std::cerr << "write failed: " << std::strerror(errno) << "\n";
+        close(socket_fd);
+        return 1;
+    }
+
+    if (shutdown(socket_fd, SHUT_WR) == -1) {
+        std::cerr << "shutdown failed: " << std::strerror(errno) << "\n";
+        close(socket_fd);
+        return 1;
+    }
+
+    char buffer[1024];
+    while (true) {
+        ssize_t bytes_read = read(socket_fd, buffer, sizeof(buffer));
+
+        if (bytes_read > 0) {
+            std::cout.write(buffer, bytes_read);
+            continue;
+        }
+
+        if (bytes_read == 0) {
+            break;
+        }
+
+        if (errno == EINTR) {
+            continue;
+        }
+
+        std::cerr << "read failed: " << std::strerror(errno) << "\n";
         close(socket_fd);
         return 1;
     }
