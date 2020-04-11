797a2dee8d4561c0d6c426998794104b5b124a65
Phuong Duong <phuongx31@gmail.com>
2020-04-11 13:02:48 +0700

    implement blocking queeu

---
+++ blocking_queue.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+#include <queue>
+
+template <typename T>
+class BlockingQueue {
+  public:
+    bool push(T value) {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (closed_) {
+                return false;
+            }
+            queue_.push(std::move(value));
+        }
+
+        cv_.notify_one();
+        return true;
+    }
+    std::optional<T> wait_and_pop() {
+        std::unique_lock<std::mutex> lock(mutex_);
+        cv_.wait(lock, [this] { return !queue_.empty() || closed_; });
+
+        if (queue_.empty()) {
+            return std::nullopt;
+        }
+
+        T item = std::move(queue_.front());
+        queue_.pop();
+        return item;
+    }
+    void close() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            closed_ = true;
+        }
+        cv_.notify_all();
+    }
+
+  private:
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::queue<T> queue_;
+    bool closed_ = false;
+};
